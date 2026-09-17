0393781ba8f1318fe5d587a04ed492be4552840e
JULIANA BARBOSA FERREIRA <164265372+Julianabf92@users.noreply.github.com>
2026-09-16 21:13:38 -0300

    Add files via upload

--- sistema.py
+++ sistema.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 class Aluno:
-    def__init__(self, n1, n2, n3):
+    def__init__(self, nome, n1, n2, n3):
     self.nome = nome 
     self.n1 = float(n1)
     self.n2 = float(n2)
@@ -21,3 +21,48 @@ class Aluno:
 
     def gerar_notas_listas(self):
         return[self.n1, self.n2, self.n3]
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+TEMPLATE_DIR = os.path.join(
+    BASE_DIR, "gerenciador_notas" , "gerenciador_notas" , "static"
+)
+STATIC_DIR = os.path.join(
+    BASE_DIR, "gerenciador_notas", "gerenciador_notas" , "static"
+)
+
+app = Flask(__name__, template_folder=Template_dir, static_folder=STATIC_DIR)
+
+@app.route("/", methods=["GET","POST"])
+def index():
+    resultado = None
+
+    if request.method == "POST":
+
+         nome = request.form.get("nome")
+         nota1 = request.form.get("n1")
+         nota2 = request.form.get("n2")
+         nota3 = request.form.get("n3")
+      
+
+         aluno = Aluno(nome, n1, n2, n3)
+
+         media = aluno.calcular_media()
+         situacao = aluno.obter_situacao()
+
+         resultado = {
+             "nome":aluno.nome,
+             "notas":aluno.gerar_notas_lista(),
+             "media":media
+             "situacao":situacao,
+         }
+
+        return render_template("index.html" , resultado=resultado)
+
+if __name__ =="__main__":
+
+  app.run(debug=True)
+
+
+
+
+
